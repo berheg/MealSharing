6fbad08f96985903ee01425c30ef7b36382269b4
Girmay <ink423103@yahoo.com>
2019-10-09 23:03:15 +0200

    init commit

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,16 +1,19 @@
+import _ from 'lodash';
 import "./index.css";
 
 import SPARouter from "@kodnificent/sparouter";
 
 import mealRouter from "./pages/meal";
 import homeRouter from "./pages/home";
+import reviewRouter from "./pages/review";
 
 const options = {
   historyMode: true // set this to true if you use the HTML5 history mode API
 };
 const router = new SPARouter(options);
 
-router.get("/", homeRouter);
-router.get("/meals/{id}", mealRouter);
+router.get("/", homeRouter(req, router));
+router.get("/meals/{id}", mealRouter(req, router));
+router.get("/review", reviewRouter(req, router));
 
 router.init();
