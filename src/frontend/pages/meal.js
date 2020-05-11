451c6e680598658494ee3d6e853e54059a616722
Girmay <ink423103@yahoo.com>
2020-05-11 21:26:15 +0200

    async wait function added

--- src/frontend/pages/meal.js
+++ src/frontend/pages/meal.js
@@ -6,15 +6,13 @@ const guestInput = document.querySelector('input.guestInput');
 const h2 = document.querySelector('h2.mealId');
 const h3 = document.querySelector('h3.formH3');
 let id;
-function mealsId(req, router) {
-  console.log(req.param.id);
-  //console.log(req.param.id);
-  getBodyContainer();  
+async function mealsId(req, router) { 
+  getBodyContainer();
   id = req.param.id;
-  //h2.innerHTML += id;
+  h2.innerHTML += id;
   const data = await fetchServer(req.param.id);
   await renderHTML(data);
-  
+
 }
 mealsId();
 formBtn.addEventListener('click', formBtnEventHandler)
@@ -24,19 +22,19 @@ listSelect.addEventListener('click', () =>searchInput.innerHTML = listSelect.val
 async function fetchServer(id){
   const res = await fetch(`/api/meals/${id}`);
   const jsonData = await res.json();
-  console.log(jsonData[0]);  
+  console.log(jsonData[0]);
   console.log(jsonData);
   return jsonData;
 }
 //renders lists of data
-function renderHTML(data){	
+function renderHTML(data){
   const ulTitle = document.querySelector('ul.mealTitle');
   const ulList = document.querySelector('ul.mealUl');
-  ulList.innerHTML = "";  
-    for(let i=0; i< data.length;i++){    
+  ulList.innerHTML = "";
+    for(let i=0; i< data.length;i++){
     const liTag = document.createElement('li');
     const liDescription = document.createElement('li');
-    const  liLocation = document.createElement('li');    
+    const  liLocation = document.createElement('li');
     liTag.innerHTML = data[i].title;
     if (i === 0){
       const liDesc = document.createElement('li');
@@ -44,67 +42,67 @@ function renderHTML(data){
       ulList.appendChild(liDesc);
     }
     liDescription.innerHTML = data[i].description;
-    liLocation.innerHTML = data[i].location;      
+    liLocation.innerHTML = data[i].location;
     ulTitle.appendChild(liTag);
     ulList.appendChild(liDescription);
     //ulList.appendChild(liLocation);
   }
-  
+
 }
 function getBodyContainer(){
   document.body.innerHTML = `
-  <div id="background" class="">  
-  <!--header start-->       
+  <div id="background" class="">
+  <!--header start-->
   <header>
-    <img class="logo" src="favicon.ico" alt="Logo of the company" 
+    <img class="logo" src="favicon.ico" alt="Logo of the company"
     />
-    <h1 class="logo-h1">ZOLLA RESTURANT</h1> 
-    <nav class="navbar">               
-      <a href="./mealId.html">Menu</a>  
+    <h1 class="logo-h1">ZOLLA RESTURANT</h1>
+    <nav class="navbar">
+      <a href="./mealId.html">Menu</a>
       <a href="">Reservations</a>
-      <a href="./review.html">Review</a>              
+      <a href="./review.html">Review</a>
     </nav>
-  </header>  <!--header end-->   
+  </header>  <!--header end-->
   <aside>
   <form action="/api/reservations" class="reservationForm">
   <h3 class="formH3">Avialable Reservations:</h3>
-  <label for="name">Reservation Form</label>      
-  <input type="number"class="phoneInput" required placeholder="Phone Number">      
-  <input type="text" class="nameInput" required placeholder="Name">      
-  <input type="email" class="emailInput" required placeholder="Email">      
+  <label for="name">Reservation Form</label>
+  <input type="number"class="phoneInput" required placeholder="Phone Number">
+  <input type="text" class="nameInput" required placeholder="Name">
+  <input type="email" class="emailInput" required placeholder="Email">
   <input type="number"class="guestInput" required placeholder="Guest Number">
   <button class="formBtn">Book Seat</button>
 </form>
-  </aside>   
+  </aside>
   <section class="bigContainer">
-    <div class="search">                
+    <div class="search">
       <input type="text" placeholder="Enter meal title" class="searchInput">
       <button class="searchBtn">Search</button>
-    </div> 
+    </div>
     <div>
     <div class="searchList">
       <ul class="searchMealList"></ul>
     </div>
-    <div class="backgroudPic">      
+    <div class="backgroudPic">
       <img src="./background/vegan.jpg" alt="background picture">
       <img src="./background/vegan2.jpg" alt="background picture">
       <img src="./background/mixed.jpg" alt="background picture">
     </div>
-    </div>  
-    <h2 class="mealId">Meal with id: </h2>    
-    <div class="mealBox">     
+    </div>
+    <h2 class="mealId">Meal with id: </h2>
+    <div class="mealBox">
       <ul  class="mealTitle">
-        <li>Title</li>        
+        <li>Title</li>
       </ul>
-      <ul id="mealList" class="mealUl"></ul>          
-    </div>   
-      
+      <ul id="mealList" class="mealUl"></ul>
+    </div>
+
   </section>
   <footer>
       <p>© 2019 Copenhagen</p>
       <p> Email: zolla-cop@gmail.dk</p>
       <p> Tlf: 40906030</p>
-  </footer>`;    
+  </footer>`;
 }
 async function formBtnEventHandler(){
   const reservation = {
@@ -130,7 +128,7 @@ async function formBtnEventHandler(){
   //search input blur event handler
 searchInput.addEventListener('blur', () =>{
   searchInput.value = '';
-  selectListDiv.style.zIndex = -2;  
+  selectListDiv.style.display = none;
 });
 //search product lists with searchkey
 function searchMealList(searchKey){
