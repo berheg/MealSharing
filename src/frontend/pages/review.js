1a55bd0cfd2217d8125e416b0ba951429c9b810f
Girmay <ink423103@yahoo.com>
2020-05-12 23:46:06 +0200

    review page modified

--- src/frontend/pages/review.js
+++ src/frontend/pages/review.js
@@ -8,13 +8,13 @@ const h3 = document.querySelector('h3.formHead');
 let id;
 async function review(req, res) {
   //console.log(req.param.id);
-  getBodyContainer();  
+  getBodyContainer();
   id = req.params.id;
   h2.innerHTML += id;
   const data = await fetchServer(3);
   await renderHTML(data);
-    
-}  
+
+}
 review();
 formBtn.addEventListener('click', formBtnEventHandler)
 //searchBtn.addEventListener('keyup',inputEventHandler);
@@ -23,60 +23,60 @@ listSelect.addEventListener('click', () =>searchInput.innerHTML = listSelect.val
 async function fetchServer(id){
   const res = await fetch(`/api/meals/${id}`);
   const jsonData = await res.json();
-  console.log(jsonData[0]);  
+  console.log(jsonData[0]);
   console.log(jsonData);
   return jsonData;
 }
 
 function getBodyContainer(){
   document.body.innerHTML = `
-  <div id="background" class="">  
-  <!--header start-->       
+  <div id="background" class="">
+  <!--header start-->
   <header>
-    <img class="logo" src="favicon.ico" alt="Logo of the company" 
+    <img class="logo" src="favicon.ico" alt="Logo of the company"
     />
-    <h1 class="logo-h1">ZOLLA RESTURANT</h1> 
-    <nav class="navbar">               
-      <a href="./mealId.html">Menu</a>  
+    <h1 class="logo-h1">ZOLLA RESTURANT</h1>
+    <nav class="navbar">
+      <a href="/meal">Menu</a>
       <a href="">Reservations</a>
-      <a href="./review.html">Review</a>              
+      <a href="/review">Review</a>
     </nav>
-  </header>  <!--header end--> 
-  <aside> 
+  </header>  <!--header end-->
+  <aside>
         <form action="/api/reviews" class="reviewForm">
             <h3 class="formHead">Fill in The Review Form </h3>
-            <label for="name">Review Form</label> 
-            <input type="text" class="titleInput" required placeholder="Title">               
+            <label for="name">Review Form</label>
+            <input type="text" class="titleInput" required placeholder="Title">
             <input type="text" class="descriptionInput" required placeholder="Description">
-            <input type="number"class="starInput" required placeholder="Star in number">      
+            <input type="number"class="starInput" required placeholder="Star in number">
             <button class="reviewFormBtn">Submit</button>
         </form>
-    </aside>     
+    </aside>
   <section class="bigContainer">
-    <div class="search">                
+    <div class="search">
       <input type="text" placeholder="Enter meal title" class="searchInput">
       <button class="searchBtn">Search</button>
     </div>
-    
+
     <div>
         <div class="searchList">
             <ul class="searchMealList"></ul>
         </div>
-        <div class="backgroudPic">      
+        <div class="backgroudPic">
             <img src="../assets/background/vegan.jpg" alt="background picture">
             <img src="../assets/background/vegan2.jpg" alt="background picture">
             <img src="../assets/background/meat.jpg" alt="background picture">
         </div>
-        
-    </div>       
-       
-    
+
+    </div>
+
+
   </section>
   <footer>
       <p>© 2019 Copenhagen</p>
       <p> Email: zolla-cop@gmail.dk</p>
       <p> Tlf: 40906030</p>
-  </footer>`;    
+  </footer>`;
 }
 async function formBtnEventHandler(){
   const reservation = {
@@ -102,7 +102,7 @@ async function formBtnEventHandler(){
   //search input blur event handler
 searchInput.addEventListener('blur', () =>{
   searchInput.value = '';
-  selectListDiv.style.zIndex = -2;  
+  selectListDiv.style.zIndex = -2;
 });
 //search product lists with searchkey
 function searchMealList(searchKey){
@@ -111,5 +111,5 @@ function searchMealList(searchKey){
     return meal.name.toLowerCase().includes(searchKey.toLowerCase())});
   console.log(searchedList);
   return searchedList;
-}  
+}
   export default review;
