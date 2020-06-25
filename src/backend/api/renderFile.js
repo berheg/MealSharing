dc55b51c7053f4f729953843361fc13f994dad88
Girmay <ink423103@yahoo.com>
2020-06-25 21:47:39 +0200

    path added

--- src/backend/api/renderFile.js
+++ src/backend/api/renderFile.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express;
 const router = express.Router();
+const path = require('path');
+//path.join(__dirname, dist);
 //router for rendering html
 router.get('/',function(req,res) {
-    res.sendFile('index.html');
+    res.sendFile(path.join(__dirname, dist),'index.html');
   });
 module.exports = router;
